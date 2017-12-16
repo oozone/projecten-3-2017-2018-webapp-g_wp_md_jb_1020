1cef136660a5ff668fbf9b1c063f90ae72f8ed86
Matthias Vanooteghem <matthias.vanooteghem@gmail.com>
2017-12-16 12:44:51 +0100

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -13,15 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
+// Players routes
 Route::put('/players/{id}/changenumber', 'Api\PlayerController@changePlayerNumber');
 Route::get('/players/{id}/match/{matchId}', 'Api\PlayerController@showMatch');
 Route::Resource('players', 'Api\PlayerController');
 
-
+// Teams routes
 Route::get('/teams/{id}/allplayers', 'Api\TeamController@getAllPlayersFromRelatedTeams');
 Route::Resource('teams', 'Api\TeamController');
 
-Route::get('/matches/mailtester', 'Api\MatchController@mailtester');
+// Matches routes
 Route::post('/matches/{id}/sign', 'Api\MatchController@signAndCloseMatch');
 Route::put('/matches/{id}/starters', 'Api\MatchController@setStarters');
 Route::put('/matches/{id}/cancel', 'Api\MatchController@cancelMatch');
@@ -29,19 +30,20 @@ Route::put('/matches/{id}/comment', 'Api\MatchController@addCommentary');
 Route::put('/matches/{id}/start', 'Api\MatchController@startMatch');
 Route::put('/matches/{id}/end', 'Api\MatchController@endMatch');
 Route::get('/matches/{id}/pdf', 'Api\MatchController@generatePdf');
-
 Route::get('/matches/active', 'Api\MatchController@activeMatches');
-
 Route::Resource('matches', 'Api\MatchController');
 
+// Divisions routes
 Route::get('/divisions/{id}/standings/{season_id}', 'Api\DivisionController@standings');
 Route::get('/divisions/{id}/topscorers', 'Api\DivisionController@topscorers');
+
+// Other resource routes
 Route::Resource('divisions', 'Api\DivisionController');
 Route::Resource('locations', 'Api\LocationController');
 Route::Resource('goals', 'Api\GoalController');
 Route::Resource('penaltybooks', 'Api\PenaltyController');
 
-
+// Authenticate for JWT-token
 Route::post('authenticate', 'Api\AuthenticateController@authenticate');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
